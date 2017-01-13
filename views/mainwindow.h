a7093d24d1b1bbaf23d94c545fb1943a343ae833
ahziel <ahziel@hope.guillaume-versal.fr>
2017-01-13 16:53:28 +0100

    Supression mainwindow exodus

--- views/mainwindow.h
+++ views/mainwindow.h
@@ -1,14 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+
 #include <QMainWindow>
-#include <QFileDialog>
-#include <QMessageBox>
-#include <processing/player.h>
-#include <QTime>
+
 
 namespace Ui {
 class MainWindow;
 }
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -17,24 +16,8 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private slots:
-    //Display video frame in player UI
-    void updatePlayerUI(QImage img);
-    //Slot for the load video push button.
-    void on_pushButton_clicked();
-    // Slot for the play push button.
-    void on_pushButton_2_clicked();
-    QString getFormattedTime(int timeInSeconds);
-
-    void on_horizontalSlider_sliderPressed();
-
-    void on_horizontalSlider_sliderReleased();
-
-    void on_horizontalSlider_sliderMoved(int position);
-
 private:
     Ui::MainWindow *ui;
-    Player* myPlayer;
 };
-#endif // MAINWINDOW_H
 
+#endif // MAINWINDOW_H
